95a1b61070c3f53c0b75310057b25e71771b1967
k92842 <k92842@gmail.com>
2025-09-24 02:22:35 +0800

    C初步檔4

---
+++ a44.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+ch=getchar();
+while(ch!='\n')
+    {
+    if(ch==' ')
+        {
+        putchar(ch);
+        }
+    else{
+        putchar(ch+1);
+        }
+    ch=getchar();
+    }
+}
