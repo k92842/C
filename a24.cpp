02879f952cdb7681c069a72dcde8b0dc7cdca9b1
k92842 <k92842@gmail.com>
2025-09-26 03:10:12 +0800

    Delete a24.cpp

--- a24.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{   printf("%s\n",__FILE__);
-    printf("%d\n",__LINE__);
-    printf("%S\n",__FUNCTION__);
-    printf("%s\n",__PRETTY_FUNCTION__);
-    printf("%s\n",__DATE__);
-printf("%s\n",__TIME__);
-return 0;}
